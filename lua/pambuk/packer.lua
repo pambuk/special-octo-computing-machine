32293e2395daa31e0c6dafb50b435b898c04bf41
pambuk <wojtek.zymonik@gmail.com>
2023-01-06 23:52:39 +0100

    Autopairs

--- lua/pambuk/packer.lua
+++ lua/pambuk/packer.lua
@@ -60,15 +60,16 @@ return require('packer').startup(function(use)
 
   use 'ThePrimeagen/harpoon'
 
-  vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-
   use {
       "nvim-neo-tree/neo-tree.nvim",
       branch = "v2.x",
-      requires = { 
+      requires = {
           "nvim-lua/plenary.nvim",
           "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
           "MunifTanjim/nui.nvim",
       }
   }
+
+  use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -144,6 +144,11 @@ _G.packer_plugins = {
     path = "/home/pambuk/.local/share/nvim/site/pack/packer/start/nui.nvim",
     url = "https://github.com/MunifTanjim/nui.nvim"
   },
+  ["nvim-autopairs"] = {
+    loaded = true,
+    path = "/home/pambuk/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
+    url = "https://github.com/windwp/nvim-autopairs"
+  },
   ["nvim-cmp"] = {
     loaded = true,
     path = "/home/pambuk/.local/share/nvim/site/pack/packer/start/nvim-cmp",
@@ -158,6 +163,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/pambuk/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
+  },
+  ["nvim-ts-autotag"] = {
+    loaded = true,
+    path = "/home/pambuk/.local/share/nvim/site/pack/packer/start/nvim-ts-autotag",
+    url = "https://github.com/windwp/nvim-ts-autotag"
   },
   ["nvim-web-devicons"] = {
     loaded = false,
